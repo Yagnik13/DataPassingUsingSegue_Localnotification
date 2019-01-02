07ee9a597e69d93f727c390817799d1166030f2f
Yagnik Suthar <yagnik.suthar@ecosmob.com>
2019-01-02 11:19:33 +0530

    Initial Commit

---
+++ DataPassingUsingSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataPassingUsingSegue
+//
+//  Created by Yagnik Suthar on 02/01/19.
+//  Copyright © 2019 ecosmob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
